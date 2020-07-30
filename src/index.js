79524b3e27278965e289a9c200a5bc5762a76d89
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-07-30 16:38:28 +0100

    Install jQuery and Popper libraries

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "jquery";
+import "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
