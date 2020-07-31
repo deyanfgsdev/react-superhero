bbb684f87a103c982a7e6b7c6f3c08c3bd32d23b
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-07-31 16:50:28 +0100

    Add redux-thunk library and redux dev tools

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
+import reduxThunk from "redux-thunk";
+
 import App from "./components/App";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
